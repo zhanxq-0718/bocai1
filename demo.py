4e88442e16b666cec13b0faa5536357346651fe9
zhanxq <zhanxq0118@outlook.com>
2024-10-28 22:11:56 +0800

    pycharm 提交的内容

---
+++ demo.py
@@ -0,0 +1,10 @@
+print("1")
+
+#pycharm + gut 相结合，需要的步骤
+#1、配置git,让 pycharm 能够使用git
+#2、配置GitHub账号（可选）
+#3、初始化仓库，相当于git init
+#4、配置忽略文件  .gitignore
+#5、添加文件到暂存区 相当于git add
+#6、添加文件到本地仓库 相当于git commit -m "xxx"
+#7、推送到远程仓库 相当于git push 远程仓库地址
